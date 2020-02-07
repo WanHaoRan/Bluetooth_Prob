fbb5b321409bff0e98cbe87cc0a636dd9af8a628
Haoran Wan <wanhr@outlook.com>
2020-02-07 21:59:12 +0800

    current location

---
+++ Bluetooth_Prob/Bluetooth_Prob/UpdateCurrentLocation.swift
@@ -0,0 +1,21 @@
+//
+//  UpdateCurrentLocation.swift
+//  Bluetooth_Prob
+//
+//  Created by 浩然  万 on 2020/2/7.
+//  Copyright © 2020 浩然  万. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateCurrentLocation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateCurrentLocation_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateCurrentLocation()
+    }
+}
